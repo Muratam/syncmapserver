c1575999635e556b2ebb2ce3c78e105eb38a6d12
murata <murata@kmc.gr.jp>
2019-09-15 14:36:50 +0900

    impl

--- syncmapserver.go
+++ syncmapserver.go
@@ -6,11 +6,12 @@ import (
 	"math/rand"
 	"net"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
-const maxClientConnectionNum = 15
+const maxSyncMapServerConnectionNum = 15
 
 // MutexInt
 type MutexInt struct {
@@ -44,76 +45,183 @@ func (this *MutexInt) Dec() int {
 	return val
 }
 
-// SyncMapServer
+// utils
+func toStringWithoutLastZero(bytes []byte) string {
+	blen := 0
+	bMaxLen := len(bytes)
+	for ; blen < bMaxLen; blen++ {
+		if bytes[blen] == 0 {
+			break
+		}
+	}
+	return string(bytes[:blen])
+}
 
-func StartSyncMapServer(port int) {
+// SyncMapServer
+// とりあえず string -> byte[] で
+type SyncMapServer struct {
+	syncmap          sync.Map
+	substanceAddress string
+	connectNum       MutexInt
+}
+
+func (m *SyncMapServer) IsOnThisApp() bool {
+	return len(m.substanceAddress) == 0
+}
+func (m *SyncMapServer) Load(key string) ([]byte, bool) {
+	val, ok := m.syncmap.Load(key)
+	if !ok {
+		return []byte(""), false
+	}
+	return val.([]byte), true
+}
+func (m *SyncMapServer) Store(key string, value []byte) {
+	m.syncmap.Store(key, value)
+}
+func NewSyncMapServer() *SyncMapServer {
+	result := &SyncMapServer{}
+	result.substanceAddress = ""
+	return result
+}
+
+// SyncMapServer
+func readAll(conn *net.Conn) []byte {
+	readMax := 1024
+	bufAll := make([]byte, readMax)
+	alreadyReadAll := false
+	if true { // 1回目だけ特別にすることで []byte のコピーを削減
+		readBufNum, err := (*conn).Read(bufAll)
+		if err != nil {
+			if err == io.EOF {
+				alreadyReadAll = true
+			} else {
+				panic(err)
+			}
+		}
+		if readBufNum < readMax {
+			alreadyReadAll = true
+			bufAll = bufAll[:readBufNum]
+		}
+	}
+	for !alreadyReadAll { // EOFまで読む
+		buf := make([]byte, readMax)
+		readBufNum, err := (*conn).Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		bufAll = append(bufAll, buf[:readBufNum]...)
+		if readBufNum < readMax { // 読み込んだ数が少ないのでもうこれ以上読み込む必要がない
+			break
+		}
+	}
+	return bufAll
+}
+
+func (this *SyncMapServer) serverImpl(conn *net.Conn) {
+	defer (*conn).Close()
+	buf := readAll(conn)
+	// 一連の要求がまとめて送られてくる。その中では整合性が保てていて欲しい。
+	command := string(buf[:4])
+	content := toStringWithoutLastZero(buf[4:])
+	// fmt.Println("Serve:", command, content)
+	if strings.Compare(command, "GET ") == 0 {
+		loaded, _ := this.Load(content)
+		conn.Write(loaded)
+		return
+	} else if strings.Compare(command, "SET ") == 0 {
+		splitted := strings.SplitN(content, "\n\n", 2) // WARN: キーに \n\nがあったら死ぬ
+		if len(splitted) != 2 {
+			conn.Write([]byte("x"))
+		} else {
+			this.Store(splitted[0], []byte(splitted[1]))
+			conn.Write([]byte("o"))
+		}
+		return
+	} else {
+
+	}
+
+}
+
+func (this *SyncMapServer) StartSyncMapServer(port int) {
 	listen, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(port))
 	defer listen.Close()
 	if err != nil {
 		panic(err)
 	}
-	var countSum MutexInt
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
 			fmt.Println("Server:", err)
 			continue
 		}
+		go this.serverImpl(&conn)
+	}
+}
+
+func (this *SyncMapServer) ConnectByClient(f func(conn net.Conn)) { // WARN: conn ptr?
+	for this.connectNum.Get() > maxSyncMapServerConnectionNum {
+		time.Sleep(time.Duration(100+rand.Intn(400)) * time.Nanosecond)
+	}
+	this.connectNum.Inc()
+	conn, err := net.Dial("tcp", this.substanceAddress)
+	if err != nil {
+		fmt.Println("Client", this.connectNum.Get(), err)
+		if conn != nil {
+			conn.Close()
+		}
+		time.Sleep(1 * time.Millisecond)
+		this.connectNum.Dec()
+		this.ConnectByClient(f)
+		return
+	}
+	f(conn)
+	conn.Close()
+	this.connectNum.Dec()
+}
+
+func testConnectByClient(substanceAddress string) {
+	var clientSyncMap = NewSyncMapServer()
+	clientSyncMap.substanceAddress = substanceAddress
+	for i := 0; i < 1; i++ {
 		go func() {
-			defer conn.Close()
-			for { // EOFまで読む
-				readMax := 1024
-				buf := make([]byte, readMax)
-				readBufNum, err := conn.Read(buf)
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					panic(err)
+			clientSyncMap.ConnectByClient(func(conn net.Conn) {
+				x := ""
+				for i := 0; i < 1025; i++ {
+					x += "a"
 				}
-				fmt.Println(countSum.Inc())
-				// fmt.Print("S:", string(buf))
-				if readBufNum < readMax { // たぶんあってるはず
-					return
-				}
-			}
+				buf := []byte(x)
+				conn.Write(buf)
+			})
+			// x := 0
+			// clientSyncMap.ConnectByClient(func(conn net.Conn) {
+			// 	buf := []byte("GET iikanji")
+			// 	conn.Write(buf)
+			// 	n, err := conn.Read(buf)
+			// 	if err != nil {
+			// 		panic(err)
+			// 	}
+			// 	x, _ = strconv.Atoi(string(buf[:n]))
+			// })
+			// fmt.Println(x)
+			// x += 1
+			// clientSyncMap.ConnectByClient(func(conn net.Conn) {
+			// 	buf := []byte("SET iikanji\n\n" + strconv.FormatInt(int64(x), 10))
+			// 	conn.Write(buf)
+			// })
 		}()
 	}
 }
 
-func testConnect(port int) {
-	var connectFunc func()
-	var connectNum MutexInt
-	connectFunc = func() {
-		for connectNum.Get() > maxClientConnectionNum {
-			time.Sleep(time.Duration(100+rand.Intn(400)) * time.Nanosecond)
-		}
-		connectNum.Inc()
-		conn, err := net.Dial("tcp", "127.0.0.1:"+strconv.Itoa(port))
-		if err != nil {
-			fmt.Println("Client", connectNum.Get(), err)
-			if conn != nil {
-				conn.Close()
-			}
-			time.Sleep(1 * time.Millisecond)
-			connectNum.Dec()
-			connectFunc()
-			return
-		}
-		buf := []byte("write")
-		conn.Write(buf)
-		conn.Close()
-		connectNum.Dec()
-	}
-	for i := 0; i < 1000; i++ {
-		go connectFunc()
-	}
-}
-
 func testSyncMapServer() {
-	go StartSyncMapServer(8888)
+	var serverSyncMap = NewSyncMapServer()
+	go serverSyncMap.StartSyncMapServer(8888)
 	time.Sleep(10 * time.Millisecond)
-	go testConnect(8888)
+	serverSyncMap.Store("iikanji", []byte("0"))
+	go testConnectByClient("127.0.0.1:8888")
 	for { // 通常はGojiとかのサブとして使うので無限に待機
 		time.Sleep(1000 * time.Millisecond)
 	}
