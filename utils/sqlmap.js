633f18371c65c7bb16b5189d0f37f28b04950ae8
murata <murata@kmc.gr.jp>
2019-09-22 18:09:56 +0900

    dot test fin

--- utils/sqlmap.js
+++ utils/sqlmap.js
@@ -1,14 +1,13 @@
-
-// 引数のパス一覧の中のGoのコード中からSQLの関係図を生成する
-// router.Get("/hoge",func(){...}) みたいなコードは全てflattenした場合を想定。
-// 1. \s*?func\s+?(\S+?)\(.*?\) を関数の始まりのラインとして解釈して、
-// 2. [`"]\s*(SELECT|DELETE|INSERT|UPDATE) .+?[`"] があるものをSQL文として解釈する。
-//   - SELECT は依存しているテーブル名を全て抜き出し、 他は...?
-// 3. SQL文を含む関数一覧が取れるので関数同士の依存関係のグラフも生成できる。
-// 4. SQLをパースして、テーブルとの関係を取得してgraphvizする。
+// NOTE: 都合上省きたいものもあるだろう！
+const ignoreFuncNames = ["main", "unsafeParseDate", "NewHandler", "InitBenchmark"]
+// NOTE: 都合上関数同士の関係を省きたいときもあるだろう！
+const ignoreFunctionRelativity = false;
+// NOTE: 見にくいレイアウトはいやだろう！ dot, fdp, neato, sfdp, twopi
+const layoutType = "dot";
 
 const fs = require("fs")
 function parseGoSQLRelation(content) {
+  content = content.replace(/\/\/.*\n/g, "")
   function escape(s) {
     return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
   }
@@ -29,13 +28,15 @@ function parseGoSQLRelation(content) {
       }
       return [commands[i + 1].replace(/[^_0-9a-zA-Z]/g, ""), queryType];
     }
-    console.error("ERROR!")
+    console.error("ERROR QUERY (splited ?)!!")
+    console.error(query)
+    return ["parse_error", queryType]
   }
   // 見つかった関数
   let textIndexToFuncName = new Array(content.length);
   let functionNames = []
   let preI = 0
-  let preName = "???"
+  let preName = "unknown_function"
   let havingFunctionsMap = {}
   for (let found of content.matchAll(/func\s+?(\S+?)\(.*?\)/g)) {
     let funcName = found[1]
@@ -53,8 +54,8 @@ function parseGoSQLRelation(content) {
   }
   // 見つかったSQL
   // SET ALTER RENAME DROP REPLACE あたりは知らない...
-  let sqlFounds = content.match(/"\s*(SELECT|DELETE|INSERT|UPDATE)\s.+?"/ig)
-  for (let other of content.match(/`\s * (SELECT | DELETE | INSERT | UPDATE) \s.+? `/ig) || []) sqlFounds.push(other)
+  let sqlFounds = content.match(/"\s*(SELECT|DELETE|INSERT|UPDATE)\s.+?"/ig) || []
+  for (let other of content.match(/`\s*(SELECT|DELETE|INSERT|UPDATE)\s.+?`/ig) || []) sqlFounds.push(other)
   preI = 0
   let functionNameToSQL = {}
   for (let sql of sqlFounds) {
@@ -76,7 +77,9 @@ function parseGoSQLRelation(content) {
     havingFunctionsMap[rootFuncName].push(foundFuncName)
   }
   for (let key in havingFunctionsMap) {
-    havingFunctionsMap[key] = Array.from(new Set(havingFunctionsMap[key]))
+    let set = new Set(havingFunctionsMap[key])
+    set.delete(key) // 自身は不要
+    havingFunctionsMap[key] = Array.from(set)
   }
   return {
     functionNames,
@@ -94,35 +97,102 @@ function joinFiles(filenames) {
 }
 function writeDot(parsed) {
   let { functionNames, havingFunctionsMap, functionNameToSQL } = parsed
-  let funcStr = ""
-  for (let func of functionNames) {
-    funcStr += `${func};`
-  }
-  let funcRel = ""
-  for (let src in havingFunctionsMap) {
-    for (let dst of havingFunctionsMap[src]) {
-      funcRel += `${src} -> ${dst};`
+  function getUnusedFunctionSet() {
+    // 雑にやってもいけるやろ
+    let unusedSet = {}
+    for (let unused of ignoreFuncNames) {
+      unusedSet[unused] = true;
     }
+    let dirty = true;
+    while (dirty) {
+      dirty = false;
+      for (let src in havingFunctionsMap) {
+        if (unusedSet[src]) continue
+        let funcs = havingFunctionsMap[src] || []
+        let sqls = functionNameToSQL[src] || []
+        // sql は絶対に必要
+        if (sqls.length > 0) continue;
+        if (funcs.length === 0) {
+          dirty = unusedSet[src] = true;
+          continue;
+        }
+        // 全ての funcs が不要なら不要
+        if (funcs.every(x => unusedSet[x])) {
+          dirty = unusedSet[src] = true;
+          continue;
+        }
+        // 全てが一つの終点しか参照していないものは不要
+        if (funcs.every(dst => unusedSet[dst] || (havingFunctionsMap[dst] || []).every(x => unusedSet[x]))) {
+          dirty = unusedSet[src] = true;
+          continue;
+        }
+      }
+    }
+    for (let src in havingFunctionsMap) {
+      if (unusedSet[src]) continue;
+      // たどり着ける先全てにSQLが無ければunused
+      let sqlExists = false;
+      let visited = {}
+      function visit(src) {
+        if (sqlExists) return;
+        if (unusedSet[src]) return;
+        if (visited[src]) return;
+        visited[src] = true;
+        let sqls = functionNameToSQL[src] || []
+        if (sqls.length > 0) sqlExists = true;
+        let funcs = havingFunctionsMap[src] || []
+        for (let dst of funcs) visit(dst);
+      }
+      visit(src)
+      if (sqlExists) continue
+      for (let v in visited) unusedSet[v] = true
+    }
+    return unusedSet
   }
+  let unusedSet = getUnusedFunctionSet()
   let tableRel = ""
+  let usedTableNames = {}
   for (let src in functionNameToSQL) {
+    if (ignoreFuncNames.includes(src)) continue;
     for (let dst of functionNameToSQL[src]) {
       // dst.query : all of query
       // dst.type  : "SELECT" / "INSERT" / ...
+      let label = `[style="bold"]`
+      usedTableNames[dst.table] = true;
       if (dst.type === "SELECT") {
-        funcRel += `${src} -> ${dst.table};`
+        tableRel += `${dst.table} -> ${src}${label};\n`
       } else { // INSERT / DELETE / UPDATE
-        funcRel += `${dst.table} -> ${src};`
+        label = `[style="bold",label="${dst.type}",color="blue",fontcolor="blue"]`
+        tableRel += `${src} -> ${dst.table}${label};\n`
       }
     }
   }
+  let tableStr = ""
+  for (let table in usedTableNames) {
+    let tableName = table;
+    if (table === "parse_error") tableName = `???`;
+    tableStr += `${table}[label="${tableName}",shape=box, style="filled, bold, rounded", fillcolor="#ffffcc"];\n`
+  }
 
+  let funcRel = ""
+  let funcStr = ""
+  if (!ignoreFunctionRelativity) {
+    for (let src in havingFunctionsMap) {
+      for (let dst of havingFunctionsMap[src]) {
+        if (unusedSet[src] || unusedSet[dst]) continue;
+        funcRel += `${dst} -> ${src};\n`
+      }
+    }
+    for (let func of functionNames) {
+      if (unusedSet[func]) continue;
+      funcStr += `${func}[label="${func}"];\n`
+    }
+  }
   return `
   digraph  {
-    layout = "neato";
-    overlap=false;
-    splines=false;
-    edge [len=0.0];
+    layout = "${layoutType}";
+    overlap = false;
+    splines = false;
     node [
       landscape = true,
       width = 1,
@@ -132,18 +202,26 @@ function writeDot(parsed) {
       fillcolor="#fafafa",
     ];
     edge [
-      len=1.8,
-      fontsize="20",
-      // penwidth="2",
+      len=0.1,
+      fontsize="8",
       fontname = "Helvetica",
       style="dashed",
     ];
+    ${tableStr}
     ${funcStr}
     ${funcRel}
     ${tableRel}
   }
   `
 }
+
+// 引数のパス一覧の中のGoのコード中からSQLの関係図を生成する
+// router.Get("/hoge",func(){...}) みたいなコードは全てflattenした場合を想定。
+// 1. \s*?func\s+?(\S+?)\(.*?\) を関数の始まりのラインとして解釈して、
+// 2. [`"]\s*(SELECT|DELETE|INSERT|UPDATE) .+?[`"] があるものをSQL文として解釈する。
+//   - SELECT は依存しているテーブル名を全て抜き出し、 他は...?
+// 3. SQL文を含む関数一覧が取れるので関数同士の依存関係のグラフも生成できる。
+// 4. SQLをパースして、テーブルとの関係を取得してgraphvizする。
 
 // 除外したい場合もあるだろうので、 `node sqlmap.js ./*.go` みたいにするのがいいかな
 let joinedContents = joinFiles(process.argv)
